106c7afb75b0849f67b41f0c6f4cb4ae5714eaa7
NLTDat20CLC <106418602+NLTDat20CLC@users.noreply.github.com>
2023-12-29 15:08:45 +0700

    Add files via upload

--- src/network.h
+++ src/network.h
@@ -7,7 +7,7 @@
 #include "./loss.h"
 #include "./optimizer.h"
 #include "./utils.h"
-
+#include <fstream>
 class Network {
  private:
   std::vector<Layer*> layers;  // layer pointers
@@ -42,6 +42,8 @@ class Network {
   /// Debugging tool to check parameter gradients
   void check_gradient(const Matrix& input, const Matrix& target, int n_points,
                       int seed = -1);
+  void save_parameters(std::string filename);
+  void load_parameters(std::string filename);
 };
 
 #endif  // SRC_NETWORK_H_
