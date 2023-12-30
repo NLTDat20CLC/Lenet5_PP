674cc9a4fd5761cd2ab11e169cd824ab919de710
NLTDat20CLC <106418602+NLTDat20CLC@users.noreply.github.com>
2023-12-30 10:17:05 +0700

    Update conv_gpu.cc

--- src/layer/conv_gpu.cc
+++ src/layer/conv_gpu.cc
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <iostream>
 
-void Conv_Custom::init() {
+void Conv_GPU::init() {
   height_out = (1 + (height_in - height_kernel + 2 * pad_h) / stride);
   width_out =   (1 + (width_in - width_kernel + 2 * pad_w) / stride);
   dim_out = height_out * width_out * channel_out;
@@ -19,7 +19,7 @@ void Conv_Custom::init() {
 }
 
 
-void Conv_Custom::forward(const Matrix& bottom) {
+void Conv_GPU::forward(const Matrix& bottom) {
   int n_sample = bottom.cols();
   top.resize(height_out * width_out * channel_out, n_sample);
   float *x = (float*)bottom.data();
@@ -51,15 +51,15 @@ void Conv_Custom::forward(const Matrix& bottom) {
 
 }
 
-void Conv_Custom::backward(const Matrix& bottom, const Matrix& grad_top) {
+void Conv_GPU::backward(const Matrix& bottom, const Matrix& grad_top) {
 
 }
 
-void Conv_Custom::update(Optimizer& opt) {
+void Conv_GPU::update(Optimizer& opt) {
 
 }
 
-std::vector<float> Conv_Custom::get_parameters() const {
+std::vector<float> Conv_GPU::get_parameters() const {
   std::vector<float> res(weight.size() + bias.size());
   // Copy the data of weights and bias to a long vector
   std::copy(weight.data(), weight.data() + weight.size(), res.begin());
@@ -67,14 +67,14 @@ std::vector<float> Conv_Custom::get_parameters() const {
   return res;
 }
 
-void Conv_Custom::set_parameters(const std::vector<float>& param) {
+void Conv_GPU::set_parameters(const std::vector<float>& param) {
   if(static_cast<int>(param.size()) != weight.size() + bias.size())
       throw std::invalid_argument("Parameter size does not match");
   std::copy(param.begin(), param.begin() + weight.size(), weight.data());
   std::copy(param.begin() + weight.size(), param.end(), bias.data());
 }
 
-std::vector<float> Conv_Custom::get_derivatives() const {
+std::vector<float> Conv_GPU::get_derivatives() const {
   std::vector<float> res(grad_weight.size() + grad_bias.size());
   // Copy the data of weights and bias to a long vector
   std::copy(grad_weight.data(), grad_weight.data() + grad_weight.size(), res.begin());
