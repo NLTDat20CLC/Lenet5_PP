de10d0450757029c3aec6ed2a7126c40e4642956
Rayxus <75016723+tuixu123@users.noreply.github.com>
2023-12-30 18:09:44 +0700

    Update gpu-utils.h

--- src/layer/gpu-utils.h
+++ src/layer/gpu-utils.h
@@ -2,18 +2,6 @@
 #define SRC_LAYER_GPU_UTILS_H
 #include <cuda_runtime.h>
 
-// Use this class to define GPU functions that students don't need access to. 
-class GPU_Utils
-{
-    public:
-    /* For creating a dummy kernel call so that we can distinguish between kernels launched for different layers
-     * in the Nsight Compute CLI for measuring per layer Op Times
-     */
-    void insert_post_barrier_kernel();
-    // For inserting a marker visible in Nsys so that we can time total student function time
-    void insert_pre_barrier_kernel();
-};
-
 struct GpuTimer
 {
     cudaEvent_t start;
