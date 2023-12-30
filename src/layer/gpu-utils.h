c096c06f8aa356e5b0726a49627400e8b69b1671
Rayxus <75016723+tuixu123@users.noreply.github.com>
2023-12-30 18:21:54 +0700

    Update gpu-utils.h

--- src/layer/gpu-utils.h
+++ src/layer/gpu-utils.h
@@ -1,6 +1,20 @@
 #ifndef SRC_LAYER_GPU_UTILS_H
 #define SRC_LAYER_GPU_UTILS_H
+#include <stdio.h>
+#include <stdint.h>
 #include <cuda_runtime.h>
+
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(1);\
+    }\
+}
 
 struct GpuTimer
 {
@@ -38,4 +52,5 @@ struct GpuTimer
         return elapsed;
     }
 };
+
 #endif 
